91ee29e99e68d5954a678bb5f1881555201d03c5
Exelo <exelo.corp@gmail.com>
2024-11-21 22:08:11 +0100

    feat(api): add documentation to auth resolver

--- apps/backend/api/src/application/http/graphql/dto/response/auth/token.auth.response.ts
+++ apps/backend/api/src/application/http/graphql/dto/response/auth/token.auth.response.ts
@@ -2,14 +2,22 @@ import { Field, ObjectType } from "@nestjs/graphql";
 
 import { IAuthToken } from "@domain/auth/dto/token.auth.type";
 
-@ObjectType()
+@ObjectType({ description: "Response type for auth" })
 export class AuthTokenResponse implements IAuthToken {
-  @Field(() => String)
+  @Field(() => String, {
+    description: "Access token for the client to use to make others requests",
+  })
   token: string;
 
-  @Field(() => String)
+  @Field(() => String, {
+    description:
+      "Refresh token for the client to use to refresh the access token when it is outdated",
+  })
   refreshToken: string;
 
-  @Field(() => Date)
+  @Field(() => Date, {
+    description:
+      "Access token expiration date in the time zone of the server (currently timezone of Paris)",
+  })
   tokenExpiresAt: Date;
 }

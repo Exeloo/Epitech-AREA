78a4c36735eb809bf3b9f4ac873601680607a42f
TIMJAsmine <timothe.jacquot@bejam.fr>
2024-12-02 17:27:44 +0100

    feat(webapp): Create auth page

---
+++ apps/frontend/web/src/routes/auth/signup/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Input from "$lib/auth/Input.svelte";
+    import Submit from "$lib/auth/Submit.svelte";
+    import Validation from "$lib/auth/Validation.svelte";
+</script>
+
+<Input title="Username" placeholder="Enter your username" dataType="text" />
+<Input title="Email" placeholder="Enter your email" dataType="email" />
+<Input title="Password" placeholder="Create your password" dataType="password" />
+<Validation description="Must be at least 8 characters"/>
+<Validation description="Must contain one special character"/>
+<Submit text="Get started"/>
+<p>
+    Already have an account?<a href="/auth/login/" class="font-semibold pl-2">Log in</a>
+</p>

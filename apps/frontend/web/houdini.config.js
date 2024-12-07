a917e73b83645ed31eb07977078275a3c07d9a9c
Tim <timothe.jacquot@epitech.eu>
2024-12-07 16:25:53 +0100

    fix(webapp): install houdini

---
+++ apps/frontend/web/houdini.config.js
@@ -0,0 +1,25 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "http://localhost:3000/graphql"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    },
+    scalars: {
+        DateTime: {
+            type: 'Date',
+            unmarshal(val) {
+                return val ? new Date(val) : null
+            },
+            marshal(date) {
+                return date && date.getTime()
+            }
+        },
+    },
+
+}
+
+export default config

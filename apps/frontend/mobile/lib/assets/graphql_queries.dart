73c2cfaa140c82215fc574b8bf9c71158e8f932e
thomas <thomas.briand@epitech.eu>
2024-12-08 15:35:33 +0100

    feat(gql): fixed requests

--- apps/frontend/mobile/lib/assets/graphql_queries.dart
+++ apps/frontend/mobile/lib/assets/graphql_queries.dart
@@ -1,25 +1,45 @@
 const String loginMutation = """
 mutation Login(\$email: String!, \$password: String!) {
-  login(email: \$email, password: \$password) {
+  login(data: {email: \$email, password: \$password}) {
     token
-    user {
-      id
-      name
-      email
-    }
+    refreshToken
+    tokenExpiresAt
   }
 }
 """;
 
 const String registerMutation = """
-mutation Register(\$username: String!, \$email: String!, \$password: String!) {
-  register(username: \$username, email: \$email, password: \$password) {
+mutation Register(\$username: String!, \$email: String!, \$password: String!, \$firstName: String!, \$lastName: String!) {
+  register(data: {username: \$username, email: \$email, password: \$password, firstName: \$firstName, lastName: \$lastName}) {
+    id
+    username
+    email
+    firstName
+    lastName
+  }
+}
+""";
+
+const String createUserMutation = """
+mutation CreateUser(\$username: String!, \$email: String!, \$password: String, \$firstName: String!, \$lastName: String!, \$description: String, \$pronoun: String) {
+  createUser(data: {username: \$username, email: \$email, password: \$password, firstName: \$firstName, lastName: \$lastName, description: \$description, pronoun: \$pronoun}) {
+    id
+    username
+    email
+    firstName
+    lastName
+    description
+    pronoun
+  }
+}
+""";
+
+const String refreshTokenMutation = """
+mutation RefreshToken(\$refreshToken: String!) {
+  refreshToken(data: {refreshToken: \$refreshToken}) {
     token
-    user {
-      id
-      name
-      email
-    }
+    refreshToken
+    tokenExpiresAt
   }
 }
 """;
@@ -28,8 +48,13 @@ const String getUserQuery = """
 query GetUser(\$id: ID!) {
   user(id: \$id) {
     id
-    name
+    username
     email
+    firstName
+    lastName
+    description
+    pronoun
+    picture
   }
 }
 """;

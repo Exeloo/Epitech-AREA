5cd0b73eddea92a88cc776974c053df67bae5748
Tim <timothe.jacquot@epitech.eu>
2024-12-08 20:23:31 +0100

    feat(webapp): calls the login and register queries to the api

--- apps/frontend/web/src/lib/auth/Checkbox.svelte
+++ apps/frontend/web/src/lib/auth/Checkbox.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-    export let overall = false;
+    interface Props {
+        overall?: boolean;
+        value: boolean,
+    }
+    const { overall=false, value = $bindable()}: Props = $props();
 </script>
 
 <div class="relative flex items-center rounded-full text-secondary">
     <input
-            id="checkbox-all-search"
-            type="checkbox"
-            value=""
-            class="peer w-5 h-5 relative z-10 rounded-md appearance-none cursor-pointer border-neutral-300 checked:bg-center border-2 checked:border-secondary checked:bg-secondary checked:bg-opacity-20">
+        id="checkbox-all-search"
+        type="checkbox"
+        value=""
+        class="peer w-5 h-5 relative z-10 rounded-md appearance-none cursor-pointer border-neutral-300 checked:bg-center border-2 checked:border-secondary checked:bg-secondary checked:bg-opacity-20">
     <label for="checkbox-all-search" class="sr-only">checkbox</label>
     {#if overall}
         <i class="fi fi-br-minus hidden absolute inset-0 items-center justify-center peer-checked:flex text-xs"></i>

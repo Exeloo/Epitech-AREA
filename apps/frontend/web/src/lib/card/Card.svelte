203211f5ef9008c71508ac3fe360a3bf0ecf8759
Tim <timothe.jacquot@epitech.eu>
2024-12-11 00:07:38 +0100

    fix(webapp): use the svelte 5 syntax for slider button

--- apps/frontend/web/src/lib/card/Card.svelte
+++ apps/frontend/web/src/lib/card/Card.svelte
@@ -1,17 +1,29 @@
 <script>
-	import Slider from "$lib/card/Slider.svelte";
+	import Slider from '$lib/card/Slider.svelte';
 </script>
 
 <div class="flex w-80 flex-col gap-16 rounded-3xl bg-[#FFB4E4] p-7">
 	<div class="flex justify-between">
 		<a href="/apps/frontend/web/static" class="font-bold">BillHarper_</a>
 		<div class="flex w-14 gap-2">
-			<a href="/apps/frontend/web/static"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="instagram" class="h-6" /></a>
-			<a href="/apps/frontend/web/static"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/1280px-Gmail_icon_%282020%29.svg.png" alt="gmail" class="h-6" /></a>
+			<a href="/apps/frontend/web/static"
+				><img
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png"
+					alt="instagram"
+					class="h-6"
+				/></a
+			>
+			<a href="/apps/frontend/web/static"
+				><img
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/1280px-Gmail_icon_%282020%29.svg.png"
+					alt="gmail"
+					class="h-6"
+				/></a
+			>
 		</div>
 	</div>
 	<p class="text-4xl font-bold drop-shadow-lg">
 		Transfer instagram DM by email with a gmail account
 	</p>
-	<Slider/>
+	<Slider />
 </div>
--- apps/frontend/web/src/lib/card/Slider.svelte
+++ apps/frontend/web/src/lib/card/Slider.svelte
@@ -1,71 +1,72 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    let isOn = $state(false);
-    let buttonWidth: number = $state(40);
-    let parentWidth = $state(0);
-    let startX: number = $state(0);
-    let offsetX: number = $state(0);
-    let isDragging = $state(false);
+	let isOn = $state(false);
+	let buttonWidth: number = $state(40);
+	let parentWidth = $state(0);
+	let startX: number = $state(0);
+	let offsetX: number = $state(0);
+	let isDragging = $state(false);
 
-    let parentDiv: HTMLElement;
-    let button: HTMLElement;
+	let parentDiv: HTMLElement;
+	let button: HTMLElement;
 
-    onMount(() => {
-        if (parentDiv) {
-            parentWidth = parentDiv.offsetWidth;
+	onMount(() => {
+		if (parentDiv) {
+			parentWidth = parentDiv.offsetWidth;
 
-            const buttonElement = parentDiv.querySelector("button");
-            if (buttonElement) {
-                buttonWidth = buttonElement.offsetWidth;
-            }
-        }
-    });
+			const buttonElement = parentDiv.querySelector('button');
+			if (buttonElement) {
+				buttonWidth = buttonElement.offsetWidth;
+			}
+		}
+	});
 
-    const startDrag = (event: MouseEvent) => {
-        isDragging = true;
-        startX = event.clientX - offsetX;
-        document.addEventListener("mousemove", onDrag);
-        document.addEventListener("mouseup", stopDrag);
-    };
+	const startDrag = (event: MouseEvent) => {
+		isDragging = true;
+		startX = event.clientX - offsetX;
+		document.addEventListener('mousemove', onDrag);
+		document.addEventListener('mouseup', stopDrag);
+	};
 
-    const onDrag = (event: MouseEvent) => {
-        if (isDragging) {
-            offsetX = event.clientX - startX;
+	const onDrag = (event: MouseEvent) => {
+		if (isDragging) {
+			offsetX = event.clientX - startX;
 
-            if (offsetX < 0) offsetX = 0;
-            if (offsetX > parentWidth - buttonWidth) offsetX = parentWidth - buttonWidth;
-        }
-    };
+			if (offsetX < 0) offsetX = 0;
+			if (offsetX > parentWidth - buttonWidth) offsetX = parentWidth - buttonWidth;
+		}
+	};
 
-    const stopDrag = () => {
-        isDragging = false;
-        document.removeEventListener("mousemove", onDrag);
-        document.removeEventListener("mouseup", stopDrag);
+	const stopDrag = () => {
+		isDragging = false;
+		document.removeEventListener('mousemove', onDrag);
+		document.removeEventListener('mouseup', stopDrag);
 
-        if (offsetX >= (parentWidth * 0.75)) {
-            isOn = true;
-        } else {
-            isOn = false;
-        }
+		if (offsetX >= parentWidth * 0.75) {
+			isOn = true;
+		} else {
+			isOn = false;
+		}
 
-        console.log(offsetX + ' > ' + parentWidth * 0.75);
-        offsetX = isOn ? parentWidth - buttonWidth - 8 : 0;
-    };
+		console.log(offsetX + ' > ' + parentWidth * 0.75);
+		offsetX = isOn ? parentWidth - buttonWidth - 8 : 0;
+	};
 
-    const toggleButton = () => {
-        isOn = !isOn;
-        offsetX = isOn ? parentWidth - buttonWidth - 8 : 0;
-    };
+	const toggleButton = () => {
+		isOn = !isOn;
+		offsetX = isOn ? parentWidth - buttonWidth - 8 : 0;
+	};
 </script>
 
 <div class="relative" bind:this={parentDiv}>
-    <div class="w-full h-12 bg-neutral-100 rounded-full"></div>
-    <button
-            bind:this={button}
-            class="absolute top-1 left-1 bg-blue-500 rounded-full shadow-md transform transition-transform duration-300 ease-in-out"
-            style="width: {buttonWidth}px; height: {buttonWidth}px; transform: translateX({offsetX}px);"
-            on:mousedown={startDrag}
-            on:click={toggleButton}
-    ></button>
+	<div class="h-12 w-full rounded-full bg-neutral-100"></div>
+	<button
+		aria-label="connect slider"
+		bind:this={button}
+		class="absolute left-1 top-1 transform rounded-full bg-blue-500 shadow-md transition-transform duration-300 ease-in-out"
+		style="width: {buttonWidth}px; height: {buttonWidth}px; transform: translateX({offsetX}px);"
+		onmousedown={startDrag}
+		onclick={toggleButton}
+	></button>
 </div>

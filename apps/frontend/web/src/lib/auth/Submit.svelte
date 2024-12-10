9dac92e35e81ff587e0882511ae2fdf87b661ac0
Exelo <exelo.corp@gmail.com>
2024-12-10 21:04:28 +0100

    feat(web): add web to monorepo

--- apps/frontend/web/src/lib/auth/Submit.svelte
+++ apps/frontend/web/src/lib/auth/Submit.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-    import type {MouseEventHandler} from "svelte/elements";
+	import type { MouseEventHandler } from 'svelte/elements';
 
-    interface Props {
-        text: string,
-        onsubmit: MouseEventHandler<HTMLButtonElement>
-    }
-    const { text, onsubmit}: Props = $props();
+	interface Props {
+		text: string;
+		onsubmit: MouseEventHandler<HTMLButtonElement>;
+	}
+	const { text, onsubmit }: Props = $props();
 </script>
 
-<button type="submit" onclick={onsubmit} class="bg-primary text-white font-bold w-full py-2 rounded-xl text-xl shadow-md shadow-gray-500">
-    {text}
+<button
+	type="submit"
+	onclick={onsubmit}
+	class="w-full rounded-xl bg-primary py-2 text-xl font-bold text-white shadow-md shadow-gray-500"
+>
+	{text}
 </button>

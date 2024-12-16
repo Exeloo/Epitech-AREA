8202cf5bfafc9e5485c17486cb270e4a19603f5d
Briand Thomas <91872942+ThomasBriand@users.noreply.github.com>
2024-12-17 00:07:05 +0100

    feat(): main not changed

--- apps/frontend/mobile/lib/main.dart
+++ apps/frontend/mobile/lib/main.dart
@@ -14,8 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(),
-      // home: MainNavigationPage(),
+      home: MainNavigationPage(),
     );
   }
 }

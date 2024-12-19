4b84ba98e392a3800fd1c4bc2c56860cde910279
ilyescmoi <ilyes.mechouar@epitech.eu>
2024-12-19 23:54:57 +0100

    fix(mobile): remove useless response variable in authHelper and in getAuthToken parameter

--- apps/frontend/mobile/lib/modules/auth/authHelper.dart
+++ apps/frontend/mobile/lib/modules/auth/authHelper.dart
@@ -2,12 +2,9 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mobile/graphql/__generated__/auth.data.gql.dart';
 
 class AuthHelper {
-  late dynamic response;
   final storage = const FlutterSecureStorage();
-  
-  
 
-  Future<String?> getAuthToken(response) async {
+  Future<String?> getAuthToken() async {
     try {
       String? authToken = await storage.read(key: "auth_token");
 

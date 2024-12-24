7289eff0a4fd2e30516ff7097bcb76ab370ea5f1
Robin Biard <robin.biard@epitech.eu>
2024-12-24 03:57:40 +0100

    feat(Deployment): add LoadBalancer and upgrade registry with it

---
+++ .cloud/public/kubernetes/services/ingress/metallb_install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Install MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/config/manifests/metallb-native.yaml
+
+# Get Machine IP
+MACHINE_IP=$(hostname -I | awk '{print $1}')
+
+# Configure MetalLB
+cat <<EOF > /tmp/metallb.yaml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: my-ip-pool
+  namespace: metallb-system
+spec:
+  addresses:
+    - ${MACHINE_IP}-${MACHINE_IP}
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: my-l2-advertisement
+  namespace: metallb-system
+EOF
+
+# Apply Config
+sleep 30
+sudo kubectl apply -f /tmp/metallb.yaml

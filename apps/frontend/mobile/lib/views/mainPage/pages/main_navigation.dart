e64f086d1450cffd1e01550fd7f300705cb5aa4a
thomas briand <thomas.briand@epitech.eu>
2024-12-26 23:51:04 +0100

    feat(): front of profile page, errors, not working

--- apps/frontend/mobile/lib/views/mainPage/pages/main_navigation.dart
+++ apps/frontend/mobile/lib/views/mainPage/pages/main_navigation.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mobile/views/home/pages/home.dart';
-import 'package:flutter/material.dart';
+import 'package:mobile/modules/graphql/repository/user_repository.dart';
+import 'package:ferry/ferry.dart';
+import 'package:provider/provider.dart';
 
 class MainNavigationPage extends StatefulWidget {
   const MainNavigationPage({super.key});
@@ -136,62 +138,170 @@ class Page3 extends StatelessWidget {
       body: Center(child: Text("Bienvenue sur la page Liste")),
     );
   }
-}class Page4 extends StatelessWidget {
+}
+
+class Page4 extends StatefulWidget {
   const Page4({super.key});
+
+  @override
+  _Page4State createState() => _Page4State();
+}
+
+class _Page4State extends State<Page4> {
+  late UserRepository userRepository;
+  GgetMeData_getMe? user;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _pronounController = TextEditingController();
+  final TextEditingController _pictureController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    final client = Provider.of<Client>(context, listen: false);
+    userRepository = UserRepository(client: client);
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    try {
+      final userData = await userRepository.getMe();
+      setState(() {
+        user = userData?.getMe;
+        if (user != null) {
+          _emailController.text = user!.email;
+          _usernameController.text = user!.username;
+          _firstNameController.text = user!.firstName;
+          _lastNameController.text = user!.lastName;
+          _descriptionController.text = user!.description ?? '';
+          _pronounController.text = user!.pronoun ?? '';
+          _pictureController.text = user!.picture ?? '';
+        }
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred: $e')),
+      );
+    }
+  }
+
+  Future<void> _updateUserData() async {
+    if (user != null) {
+      try {
+        await userRepository.updateUser(
+          id: user!.id,
+          email: _emailController.text,
+          username: _usernameController.text,
+          firstName: _firstNameController.text,
+          lastName: _lastNameController.text,
+          description: _descriptionController.text,
+          pronoun: _pronounController.text,
+          picture: _pictureController.text,
+        );
+        _fetchUserData();
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('An error occurred: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          bool isPortrait = constraints.maxWidth < constraints.maxHeight;
-
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: isPortrait ? 50 : 30,
-                    backgroundImage: const NetworkImage('https://via.placeholder.com/150'),
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    'John Doe',
-                    style: TextStyle(
-                      fontSize: isPortrait ? 24 : 18,
-                      fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: user == null
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(user!.picture ?? 'https://via.placeholder.com/150'),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'john.doe@example.com',
-                    style: TextStyle(
-                      fontSize: isPortrait ? 16 : 14,
-                      color: Colors.grey,
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Edit Profile button pressed');
-                    },
-                    child: const Text('Edit Profile'),
-                  ),
-                  const SizedBox(height: 8),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Logout button pressed');
-                    },
-                    child: const Text('Logout'),
-                  ),
-                ],
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _firstNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'First Name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _lastNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Last Name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _descriptionController,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _pronounController,
+                      decoration: const InputDecoration(
+                        labelText: 'Pronoun',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: _pictureController,
+                      decoration: const InputDecoration(
+                        labelText: 'Picture URL',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: _updateUserData,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        textStyle: const TextStyle(fontSize: 16),
+                        backgroundColor: const Color(0xff8E44AD),
+                      ),
+                      child: const Text(
+                        'Update Profile',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
       ),
     );
   }

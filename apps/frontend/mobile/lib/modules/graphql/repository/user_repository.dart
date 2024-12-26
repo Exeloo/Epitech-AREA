3b2c1db3b9b4cf5b053525254bcb7badc401e348
thomas briand <thomas.briand@epitech.eu>
2024-12-26 23:51:46 +0100

    feat(): repository finished should work, but not working, getMe & updateUser implemented

--- apps/frontend/mobile/lib/modules/graphql/repository/user_repository.dart
+++ apps/frontend/mobile/lib/modules/graphql/repository/user_repository.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:ferry/ferry.dart';
 import 'package:mobile/graphql/__generated__/user.data.gql.dart';
-import 'package:mobile/graphql/__generated__/user.req.gql.dart';
+import 'package:mobile/graphql/__generated__/user.req.gql.dart';!
 
 class UserRepository {
   final Client client;
@@ -58,6 +58,44 @@ class UserRepository {
       return response.data;
     } catch (e) {
       log('GetMe error: $e');
+      rethrow;
+    }
+  }
+
+  Future<GupdateUserData?> updateUser({
+    required String id,
+    required String email,
+    required String username,
+    required String firstName,
+    required String lastName,
+    String? description,
+    String? pronoun,
+    String? picture,
+  }) async {
+    final updateUserReq = GupdateUserReq((b) => b
+      ..vars.data.id = id
+      ..vars.data.email = email
+      ..vars.data.username = username
+      ..vars.data.firstName = firstName
+      ..vars.data.lastName = lastName
+      ..vars.data.description = description
+      ..vars.data.pronoun = pronoun
+      ..vars.data.picture = picture);
+
+    try {
+      final response = await client.request(updateUserReq).first;
+
+      if (response.loading) {
+        log('Loading...');
+      } else if (response.hasErrors) {
+        log('Errors: ${response.graphqlErrors}');
+      } else {
+        log('Response: ${response.data}');
+      }
+
+      return response.data;
+    } catch (e) {
+      log('UpdateUser error: $e');
       rethrow;
     }
   }

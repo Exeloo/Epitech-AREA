42667133f37d29dc5f94db43846b3c4366615a9a
ilyescmoi <ilyes.mechouar@epitech.eu>
2024-12-27 01:40:57 +0100

    feat(mobile): add provider list and providerDescriptionPage on tap

--- apps/frontend/mobile/lib/views/home/widgets/provider_card.dart
+++ apps/frontend/mobile/lib/views/home/widgets/provider_card.dart
@@ -3,35 +3,70 @@ import 'package:flutter/material.dart';
 class ProviderCard extends StatelessWidget {
   final String logoUrl;
   final String providerName;
+  final int id;
+  final String color;
 
-  const ProviderCard({super.key,
+  const ProviderCard({
     required this.logoUrl,
     required this.providerName,
+    required this.id,
+    required this.color,
+    super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Container(
-          width: 300,
-          height: 400,
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            image: DecorationImage(
-              image: NetworkImage(logoUrl),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProviderDescription(id: id),
+          ),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        color: Color(int.parse(color.replaceFirst('#', '0xff'))),
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 80,
+            width: 80,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
+              child: Image.network(
+                logoUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 20),
-            ],
-          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProviderDescription extends StatelessWidget {
+  final int id;
+
+  const ProviderDescription({required this.id, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff1B1B1B),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff1B1B1B),
+        iconTheme: const IconThemeData(color: Colors.white, size: 50),
+      ),
+      body: Center(
+        child: Text(
+          'Details of Provider with ID: $id',
+          style: const TextStyle(fontSize: 20, color: Colors.white),
         ),
       ),
     );

95e9182cfee269b3882968c382d464068f81703b
Robin Biard <robin.biard@epitech.eu>
2024-12-27 02:10:50 +0100

    fix(Deployment): change docker-compose by docker images

--- .cloud/public/kubernetes/services/argocd/install.sh
+++ .cloud/public/kubernetes/services/argocd/install.sh
@@ -2,3 +2,7 @@
 
 # Install Argo-CD
 kubectl apply -n deployment -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Add permissions
+kubectl create clusterrole metallb-reader --verb=get,list,watch --resource=l2advertisements.metallb.io
+kubectl create clusterrolebinding argocd-metallb-reader --clusterrole=metallb-reader --serviceaccount=deployment:argocd-application-controller

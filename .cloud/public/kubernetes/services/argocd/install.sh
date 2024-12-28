0a0694a3ef26bbd3dc2198b78ba4b02093bc4a13
Robin Biard <robin.biard@epitech.eu>
2024-12-28 01:16:01 +0100

    fix(Deployment): push docker images in job push

--- .cloud/public/kubernetes/services/argocd/install.sh
+++ .cloud/public/kubernetes/services/argocd/install.sh
@@ -4,5 +4,5 @@
 kubectl apply -n deployment -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Add permissions
-kubectl create clusterrole metallb-reader --verb=get,list,watch --resource=l2advertisements.metallb.io
-kubectl create clusterrolebinding argocd-metallb-reader --clusterrole=metallb-reader --serviceaccount=deployment:argocd-application-controller
+#kubectl create clusterrole metallb-reader --verb=get,list,watch --resource=l2advertisements.metallb.io
+#kubectl create clusterrolebinding argocd-metallb-reader --clusterrole=metallb-reader --serviceaccount=deployment:argocd-application-controller

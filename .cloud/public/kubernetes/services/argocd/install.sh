145a4a73f580ecdd3ac75b2e50266f99a136337a
Robin Biard <robin.biard@epitech.eu>
2024-12-28 02:12:26 +0100

    fix(Deployment): only push docker images created by docker-compose

--- .cloud/public/kubernetes/services/argocd/install.sh
+++ .cloud/public/kubernetes/services/argocd/install.sh
@@ -4,5 +4,6 @@
 kubectl apply -n deployment -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Add permissions
-#kubectl create clusterrole metallb-reader --verb=get,list,watch --resource=l2advertisements.metallb.io
-#kubectl create clusterrolebinding argocd-metallb-reader --clusterrole=metallb-reader --serviceaccount=deployment:argocd-application-controller
+sleep 30
+kubectl apply -f /tmp/kubernetes/services/argocd/clusterrole.yml
+kubectl apply -f /tmp/kubernetes/services/argocd/clusterrolebinding.yml

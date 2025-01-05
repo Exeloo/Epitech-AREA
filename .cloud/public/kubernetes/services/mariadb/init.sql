caa5fbc2585accac9becf3b7ec42817e3c61dedb
Robin Biard <robin.biard@epitech.eu>
2025-01-05 01:31:05 +0100

    style(providers): fix lint

--- .cloud/public/kubernetes/services/mariadb/init.sql
+++ .cloud/public/kubernetes/services/mariadb/init.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS `area-api-db`;
 CREATE DATABASE IF NOT EXISTS `area-provider-discord-db`;
 CREATE DATABASE IF NOT EXISTS `area-provider-twitch-db`;
 
-CREATE USER IF NOT EXISTS `api` IDENTIFIED BY "admin1234";
-CREATE USER IF NOT EXISTS `provider-discord` IDENTIFIED BY "admin1234";
+CREATE USER IF NOT EXISTS `api` IDENTIFIED BY "api";
+CREATE USER IF NOT EXISTS `provider-discord` IDENTIFIED BY "provider-discord";
 CREATE USER IF NOT EXISTS `provider-twitch` IDENTIFIED BY "provider-twitch";
 
-GRANT ALL PRIVILEGES ON `area-api-db`.* TO `api` IDENTIFIED BY "admin1234";
-GRANT ALL PRIVILEGES ON `area-provider-discord-db`.* TO `provider-discord` IDENTIFIED BY "admin1234";
+GRANT ALL PRIVILEGES ON `area-api-db`.* TO `api` IDENTIFIED BY "api";
+GRANT ALL PRIVILEGES ON `area-provider-discord-db`.* TO `provider-discord` IDENTIFIED BY "provider-discord";
 GRANT ALL PRIVILEGES ON `area-provider-twitch-db`.* TO `provider-twitch` IDENTIFIED BY "provider-twitch";

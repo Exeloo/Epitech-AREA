4f5f3c09f43e777de067f0b5be2b7bd2b4c554bc
Exelo <exelo.corp@gmail.com>
2025-01-05 02:09:42 +0100

    feat(api): add twitch to sql files

--- .cloud/public/kubernetes/services/mariadb/init.sql
+++ .cloud/public/kubernetes/services/mariadb/init.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE IF NOT EXISTS `area-api-db`;
 CREATE DATABASE IF NOT EXISTS `area-provider-discord-db`;
+CREATE DATABASE IF NOT EXISTS `area-provider-twitch-db`;
 
 CREATE USER IF NOT EXISTS `api` IDENTIFIED BY "api";
 CREATE USER IF NOT EXISTS `provider-discord` IDENTIFIED BY "provider-discord";
+CREATE USER IF NOT EXISTS `provider-twitch` IDENTIFIED BY "provider-twitch";
 
 GRANT ALL PRIVILEGES ON `area-api-db`.* TO `api` IDENTIFIED BY "api";
 GRANT ALL PRIVILEGES ON `area-provider-discord-db`.* TO `provider-discord` IDENTIFIED BY "provider-discord";
+GRANT ALL PRIVILEGES ON `area-provider-twitch-db`.* TO `provider-twitch` IDENTIFIED BY "provider-twitch";

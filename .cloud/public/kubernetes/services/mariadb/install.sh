2ee1e337019eb8afb0aeedf1ca9e4478468e7c89
Robin Biard <robin.biard@epitech.eu>
2025-01-07 18:27:37 +0100

    fix(kubernetes): change namespace for secret db-password

--- .cloud/public/kubernetes/services/mariadb/install.sh
+++ .cloud/public/kubernetes/services/mariadb/install.sh
@@ -3,7 +3,7 @@
 # Create secrets for mariadb
 kubectl create secret generic mariadb-password --from-literal=mariadb-root-password=$MARIADB_PASSWORD
 
-kubectl create secret generic db-password --from-literal=database_password=$DATABASE_PASSWORD
+kubectl create secret generic db-password --from-literal=database_password=$DATABASE_PASSWORD -n deployment
 
 # Create a ConfigMap for init mariadb
 kubectl create configmap mariadb-init-sql --from-file=/tmp/kubernetes/services/mariadb/init.sql

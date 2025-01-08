301461337fa80d2105d8f67eeb2afffede2c00e6
Robin Biard <robin.biard@epitech.eu>
2025-01-08 02:09:26 +0100

    feat(kubernetes): add provider twitch in mariadb

--- .cloud/public/kubernetes/services/mariadb/init.sql
+++ .cloud/public/kubernetes/services/mariadb/init.sql
@@ -6,6 +6,6 @@ CREATE USER IF NOT EXISTS `api` IDENTIFIED BY "api";
 CREATE USER IF NOT EXISTS `provider-discord` IDENTIFIED BY "provider-discord";
 CREATE USER IF NOT EXISTS `provider-twitch` IDENTIFIED BY "provider-twitch";
 
-GRANT ALL PRIVILEGES ON `area-api-db`.* TO `api` IDENTIFIED BY "api";
-GRANT ALL PRIVILEGES ON `area-provider-discord-db`.* TO `provider-discord` IDENTIFIED BY "provider-discord";
+GRANT ALL PRIVILEGES ON `area-api-db`.* TO `api` IDENTIFIED BY "admin1234";
+GRANT ALL PRIVILEGES ON `area-provider-discord-db`.* TO `provider-discord` IDENTIFIED BY "admin1234";
 GRANT ALL PRIVILEGES ON `area-provider-twitch-db`.* TO `provider-twitch` IDENTIFIED BY "provider-twitch";

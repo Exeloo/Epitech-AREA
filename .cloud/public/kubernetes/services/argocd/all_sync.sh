49f576aacf6dad26a97d79e15aeafbb8eb1de326
Robin Biard <robin.biard@epitech.eu>
2025-01-09 23:56:25 +0100

    feat(kubernetes): add google provider in script to update apps

--- .cloud/public/kubernetes/services/argocd/all_sync.sh
+++ .cloud/public/kubernetes/services/argocd/all_sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-APPS="aether-api aether-provider-discord aether-provider-twitch aether-webapp aether-mobile"
+APPS="aether-api aether-provider-discord aether-provider-twitch aether-provider-google aether-webapp aether-mobile"
 
 for APP in $APPS; do
   kubectl rollout restart deployment $APP -n deployment

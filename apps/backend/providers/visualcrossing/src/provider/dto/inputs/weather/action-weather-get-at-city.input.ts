49d71da9428dc9ec0f334939172a82fdb66d0f48
Exelo <exelo.corp@gmail.com>
2025-01-11 22:21:07 +0100

    style(providers): fix lint

--- apps/backend/providers/visualcrossing/src/provider/dto/inputs/weather/action-weather-get-at-city.input.ts
+++ apps/backend/providers/visualcrossing/src/provider/dto/inputs/weather/action-weather-get-at-city.input.ts
@@ -1,11 +1,15 @@
-import {ManifestProperty, ManifestPropertyEnum, ManifestType,} from "@lib/manifest";
+import {
+  ManifestProperty,
+  ManifestPropertyEnum,
+  ManifestType,
+} from "@lib/manifest";
 
 @ManifestType()
 export class ActionWeatherGetAtCityInput {
   @ManifestProperty({
     type: ManifestPropertyEnum.STRING,
     name: "City",
-    description: "City to see the weather in"
+    description: "City to see the weather in",
   })
   city: string;
 }

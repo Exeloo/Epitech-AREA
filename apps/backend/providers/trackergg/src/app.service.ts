059ff90278f28a26882810e50317a4be30422510
Exelo <exelo.corp@gmail.com>
2025-01-11 23:53:58 +0100

    feat(trackergg-provider): add trackergg provider

---
+++ apps/backend/providers/trackergg/src/app.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from "@nestjs/common";
+
+import {generateManifest, IBaseManifest, ManifestOAuthEnum,} from "@lib/manifest";
+
+import {AuthService} from "~/provider/shared/auth/auth.service";
+
+const APP_BASE_MANIFEST: IBaseManifest = {
+  id: "trackergg",
+  name: "Tracker.gg",
+  description: "Provider to use the Tracker.gg API",
+  img: "",
+  color: "#ffffff",
+  oauth: {
+    type: ManifestOAuthEnum.TOKEN,
+  },
+};
+
+@Injectable()
+export class AppService {
+  constructor(private readonly authService: AuthService) {}
+
+  async getManifest(): Promise<any> {
+    return generateManifest(APP_BASE_MANIFEST);
+  }
+}
---
+++ apps/backend/providers/trackergg/src/provider/actions/csgo.action.ts
@@ -0,0 +1,41 @@
+import {HttpService} from "@nestjs/axios";
+import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+
+import {ManifestAction} from "@lib/manifest";
+
+import {BaseHttpRepository} from "~/provider/common/base-http.repository";
+import {ActionCsgoGetProfileInput} from "~/provider/dto/inputs/csgo/action-csgo-get-profile.input";
+import {ActionCsgoGetProfileResponse} from "~/provider/dto/responses/csgo/action-csgo-get-profile.response";
+import {IActionResponse} from "~/provider/services/action.service";
+import {AuthService} from "~/provider/shared/auth/auth.service";
+
+@Injectable()
+export class CsgoAction extends BaseHttpRepository {
+  constructor(
+    httpService: HttpService,
+    configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super(httpService, configService);
+  }
+
+  @ManifestAction({
+    id: "csgo-get-profile",
+    name: "CSGO Get Profile",
+    description: "Action to Get Profile of csgo",
+    img: "",
+    color: "#ffffff",
+    input: ActionCsgoGetProfileInput,
+    output: ActionCsgoGetProfileResponse,
+  })
+  userGetProfile(
+    input: ActionCsgoGetProfileInput,
+  ): IActionResponse<ActionCsgoGetProfileResponse> {
+    return this.get(`/csgo/standard/profile/steam/${input.user_id}`, {
+      headers: {
+        "TRN-Api-Key": this.authService.getToken(),
+      },
+    });
+  }
+}

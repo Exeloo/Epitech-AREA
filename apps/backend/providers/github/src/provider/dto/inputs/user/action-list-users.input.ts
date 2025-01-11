b2e78fce74214393064250869bfc0c3df59fc146
thomas briand <thomas.briand@epitech.eu>
2025-01-11 20:16:56 +0100

    feat: add git actions

---
+++ apps/backend/providers/github/src/provider/dto/inputs/user/action-list-users.input.ts
@@ -0,0 +1,23 @@
+import {
+  ManifestProperty,
+  ManifestPropertyEnum,
+  ManifestType,
+} from "@lib/manifest";
+
+@ManifestType()
+export class ActionListUsersInput {
+  @ManifestProperty({
+    type: ManifestPropertyEnum.STRING,
+  })
+  accept: string = "application/vnd.github+json";
+
+  @ManifestProperty({
+    type: ManifestPropertyEnum.NUMBER,
+  })
+  since?: number;
+
+  @ManifestProperty({
+    type: ManifestPropertyEnum.NUMBER,
+  })
+  per_page?: number = 30;
+}

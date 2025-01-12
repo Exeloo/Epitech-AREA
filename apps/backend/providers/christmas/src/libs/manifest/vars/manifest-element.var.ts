d4a4de43455dc620e302067920b98ae09e32cdb4
Exelo <exelo.corp@gmail.com>
2025-01-12 01:50:21 +0100

    feat(christmas-provider): add christmas provider

---
+++ apps/backend/providers/christmas/src/libs/manifest/vars/manifest-element.var.ts
@@ -0,0 +1,8 @@
+import {
+  IManifestActionInfo,
+  IManifestTriggerInfo,
+} from "../types/manifest-element.type";
+
+export const manifestTriggerVar = new Set<IManifestTriggerInfo>();
+
+export const manifestActionVar = new Set<IManifestActionInfo>();

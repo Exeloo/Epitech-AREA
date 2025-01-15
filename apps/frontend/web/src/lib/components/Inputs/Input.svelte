473c31d736b3fabc69ebc71c2e688ab20acc4faf
bill <timothe.jacquot@epitech.eu>
2025-01-15 21:33:46 +0100

    feat(web): add inputs on profile settings page

--- apps/frontend/web/src/lib/components/Inputs/Input.svelte
+++ apps/frontend/web/src/lib/components/Inputs/Input.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	interface Props {
 		title: string;
-		placeholder: string;
-		dataType?: 'text' | 'email' | 'password';
+		placeholder?: string;
+		dataType?: 'text' | 'email' | 'password' | 'textarea';
 		value: string;
 	}
 	let { title, placeholder, dataType = 'text', value = $bindable() }: Props = $props();
@@ -11,12 +11,22 @@
 <div class="w-full">
 	<label class="flex flex-col gap-1 font-medium text-neutral-700 dark:text-neutral-200">
 		{title}
-		<input
-			bind:value
-			class="rounded-xl bg-neutral-50 px-4 py-3 placeholder-neutral-400 shadow-inner outline outline-1 outline-gray-300 dark:bg-gray-500 dark:placeholder-neutral-200 dark:outline-gray-700"
-			name={title}
-			type={dataType}
-			{placeholder}
-		/>
+		{#if dataType === 'textarea'}
+			<textarea
+				bind:value
+				rows="4"
+				class="rounded-xl bg-neutral-50 px-4 py-3 text-sm placeholder-neutral-400 shadow-inner outline outline-1 outline-gray-300 dark:bg-gray-600 dark:font-semibold dark:text-white dark:outline-gray-800"
+				name={title}
+				{placeholder}
+			></textarea>
+		{:else}
+			<input
+				bind:value
+				class="rounded-xl bg-neutral-50 px-4 py-3 placeholder-neutral-400 shadow-inner outline outline-1 outline-gray-300 dark:bg-gray-600 dark:font-semibold dark:text-white dark:outline-gray-800"
+				name={title}
+				type={dataType}
+				{placeholder}
+			/>
+		{/if}
 	</label>
 </div>
--- apps/frontend/web/src/routes/settings/profile/+page.svelte
+++ apps/frontend/web/src/routes/settings/profile/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Input from '$lib/components/Inputs/Input.svelte';
-	import { load_getMe, UserStore } from '$houdini';
+	import { load_getMe, type User$data, type User$input, UserStore } from '$houdini';
 	import { onMount } from 'svelte';
+	import type { Readable } from 'svelte/store';
 
 	let userStore: UserStore = new UserStore();
 
-	let info;
+	let info: Readable<User$data | null> & { variables: User$input; kind: any } & {};
 
 	onMount(async () => {
 		const query = await load_getMe({});
@@ -19,5 +20,14 @@
 </script>
 
 {#if $info}
-	<Input title="Username" value={$info.username} />
+	<div class="flex flex-col gap-5">
+		<Input title="Username" value={$info.username} />
+		<Input title="Email" value={$info.email} />
+		<div class="flex gap-8">
+			<Input title="Firstname" value={$info.firstName} />
+			<Input title="Lastname" value={$info.lastName} />
+		</div>
+		<Input title="Description" dataType="textarea" value={$info.description || ''} />
+		<button class="rounded-full bg-purple-800 py-2 text-xl font-bold">Save</button>
+	</div>
 {/if}

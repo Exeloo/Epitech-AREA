237608f308d1bf5b67e2c010b1d25f308a718baa
bill <timothe.jacquot@epitech.eu>
2025-01-16 01:01:45 +0100

    feat(web): update user in settings page

--- apps/frontend/web/src/lib/components/settings/Theme.svelte
+++ apps/frontend/web/src/lib/components/settings/Theme.svelte
@@ -9,20 +9,13 @@
 	let { name, img }: Props = $props();
 
 	const handleSelection = () => {
-		if (get(selectedTheme) === name) {
-			selectedTheme.set(null);
-		} else {
+		if (get(selectedTheme) !== name) {
 			selectedTheme.set(name);
 		}
 	};
-
-	let isSelected = $derived(get(selectedTheme) === name);
 </script>
 
-<button class="rounded dark:bg-gray-700" onclick={handleSelection}>
-	<img src={img} alt={name} class="rounded-xl outline outline-gray-300" />
-	<span class="flex">
-		<input type="checkbox" checked={isSelected} readonly />
-		<span>{name}</span>
-	</span>
+<button class="space-y-2 rounded font-semibold dark:bg-gray-700" onclick={handleSelection}>
+	<img src={img} alt={name} class="rounded-xl outline outline-gray-300 dark:outline-gray-900" />
+	{name}
 </button>
--- apps/frontend/web/src/routes/settings/profile/+page.svelte
+++ apps/frontend/web/src/routes/settings/profile/+page.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import Input from '$lib/components/Inputs/Input.svelte';
-	import { load_getMe, type User$data, type User$input, UserStore } from '$houdini';
+	import { load_getMe, updateMeStore, type User$data, type User$input, UserStore } from '$houdini';
 	import { onMount } from 'svelte';
 	import type { Readable } from 'svelte/store';
 
 	let userStore: UserStore = new UserStore();
+	const updateStore = new updateMeStore();
 
 	let info: Readable<User$data | null> & { variables: User$input; kind: any } & {};
+
+	async function save() {
+		if (!$info) return;
+
+		try {
+			const query = await updateStore.mutate({
+				data: {
+					username: $info.username,
+					firstName: $info.firstName,
+					lastName: $info.lastName,
+					email: $info.email,
+					description: $info.description
+				}
+			});
+			console.log(query);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 	onMount(async () => {
 		const query = await load_getMe({});
@@ -21,13 +41,17 @@
 
 {#if $info}
 	<div class="flex flex-col gap-5">
-		<Input title="Username" value={$info.username} />
-		<Input title="Email" value={$info.email} />
+		<Input title="Username" bind:value={$info.username} />
+		<Input title="Email" bind:value={$info.email} />
 		<div class="flex gap-8">
-			<Input title="Firstname" value={$info.firstName} />
-			<Input title="Lastname" value={$info.lastName} />
+			<Input title="Firstname" bind:value={$info.firstName} />
+			<Input title="Lastname" bind:value={$info.lastName} />
 		</div>
-		<Input title="Description" dataType="textarea" value={$info.description || ''} />
-		<button class="rounded-full bg-purple-800 py-2 text-xl font-bold">Save</button>
+		{#if $info.description}
+			<Input title="Description" dataType="textarea" bind:value={$info.description} />
+		{/if}
+		<button onclick={save} class="rounded-xl bg-purple-800 py-2 text-xl font-bold shadow"
+			>Save</button
+		>
 	</div>
 {/if}

08173ca38363f864218c17ca9692a23d47b71c90
Exelo <exelo.corp@gmail.com>
2025-01-17 01:39:44 +0100

    feat(api): add triggers and actions counts on documentation

--- apps/backend/api/src/domain/documentation/documentation.type.ts
+++ apps/backend/api/src/domain/documentation/documentation.type.ts
@@ -14,12 +14,17 @@ export interface IProviderActionDocumentation {
 
 export interface IProviderDocumentation {
   name: string;
+  actions_nb: number;
+  reactions_nb: number;
   actions: IProviderTriggerDocumentation[];
   reactions: IProviderActionDocumentation[];
 }
 
 export interface IServerDocumentation {
   current_time: number;
+  actions_nb: number;
+  reactions_nb: number;
+  areas_nb: number;
   services: IProviderDocumentation[];
 }
 

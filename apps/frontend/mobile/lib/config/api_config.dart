0367383c059e05042938a1e9315d9d62b68c68ae
ilyescmoi <ilyes.mechouar@epitech.eu>
2025-01-17 01:53:09 +0100

    feat(mobile): oauth url with choose api

--- apps/frontend/mobile/lib/config/api_config.dart
+++ apps/frontend/mobile/lib/config/api_config.dart
@@ -1,7 +1,8 @@
 class AppConfig {
   static String apiUrl = 'https://api.aether-area.fr/graphql';
-
+  static String oauthUrl = 'https://api.aether-area.fr';
   static void setApiUrl(String url) {
-    apiUrl = url;
+    apiUrl = '$url/graphql';
+    oauthUrl = url;
   }
 }

148174d1188884c1c021ed304c6e1c998e976a7a
Robin Biard <robin.biard@epitech.eu>
2025-01-18 18:17:36 +0100

    fix(Riot): change image for Riot provider

--- apps/backend/providers/riot/src/app.service.ts
+++ apps/backend/providers/riot/src/app.service.ts
@@ -10,8 +10,8 @@ const APP_BASE_MANIFEST: IBaseManifest = {
   id: "riot",
   name: "Riot",
   description: "Provider to use the Riot API",
-  img: "https://firebasestorage.googleapis.com/v0/b/area-444018.firebasestorage.app/o/image-riot.png?alt=media&token=e5651f4d-bc40-4e11-97c6-174c8755312f",
-  color: "#d32f2e",
+  img: "https://firebasestorage.googleapis.com/v0/b/area-444018.firebasestorage.app/o/logo-riot.png?alt=media&token=b3d140d7-f656-403b-9750-18d6e3ed36d3",
+  color: "#eb0029",
   oauth: {
     type: ManifestOAuthEnum.NONE,
   },

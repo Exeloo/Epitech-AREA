baac0b7244c5297170d8db97bc19874d7f630e4c
Exelo <exelo.corp@gmail.com>
2025-01-19 16:56:13 +0100

    fix(web): change svg to white

--- apps/frontend/web/src/lib/components/explore/card/ActionCard.svelte
+++ apps/frontend/web/src/lib/components/explore/card/ActionCard.svelte
@@ -19,10 +19,16 @@
 	<Card color={action.color}>
 		<div class="flex h-full flex-col items-center justify-center gap-6 text-white">
 			{#if action.img}
-				<img src={action.img} alt={`Image describe ${action.name} Action`} class="h-20 w-20" />
+				<img src={action.img} alt={`Image describe ${action.name} Action`} class="h-20 w-20 img-svg" />
 			{/if}
 			<h3 class="text-3xl font-bold">{action.name}</h3>
 			<p class="">{action.description}</p>
 		</div>
 	</Card>
 {/if}
+
+<style>
+	.img-svg {
+		filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(192deg) brightness(105%) contrast(101%);
+	}
+</style>
